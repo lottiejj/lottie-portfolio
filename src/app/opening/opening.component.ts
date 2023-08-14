20fa8168a8174ddeff2f01666f4f5b09d7abd38e
Lottie <char.jon3s@gmail.com>
2023-08-14 19:03:44 +0100

    styled

---
+++ src/app/opening/opening.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-opening',
+  templateUrl: './opening.component.html',
+  styleUrls: ['./opening.component.scss']
+})
+export class OpeningComponent {
+  constructor(private router: Router) {
+
+  }
+navToHome() {
+  console.log('NAV')
+  this.router.navigateByUrl('/home');
+}
+}
